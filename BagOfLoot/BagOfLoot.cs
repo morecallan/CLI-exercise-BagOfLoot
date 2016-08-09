35abd888318e53664d14abd63cd071c7206163d1
Callan Morrison <callan.l.morrison@gmail.com>
2016-08-08 19:30:32 -0500

    instructions in file in bagofloot class

---
+++ BagOfLoot/BagOfLoot.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BagOfLoot
+{
+    public class BagOfLoot
+    {
+
+        //Add a toy to the bag o' loot, and label it with the child's name who will receive it.
+            //BagOLoot.exe add kite suzy
+            //BagOLoot.exe add baseball michael
+
+        //Remove a toy from the bag o' loot in case a child's status changes before delivery starts.
+            //BagOLoot.exe remove suzy kite
+            //BagOLoot.exe remove michael baseball
+
+        //Produce a list of children currently receiving presents.
+            //BagOLoot.exe ls
+
+        //List toys in the bag o' loot for a specific child.
+            //BagOLoot.exe ls suzy
+
+        //Specify when a child's toys have been delivered.
+            //BagOLoot.exe delivered suzy
+
+
+
+
+    }
+}
